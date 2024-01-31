cb1b2581181373c47863dde8646196ca3d3ca330
dsmuhilan <125954828+dsmuhilan@users.noreply.github.com>
2024-01-31 11:37:30 +0530

    Add files via upload

---
+++ frontend/Day5/src/App.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/contact";
+import Navbar from "./components/NavBar"; 
+import Login from "./pages/Login";
+import UserPage from "./pages/Courses";
+import Signup from "./pages/Signuppage";
+import Adminpanel from "./pages/Adminpanel";
+import Profile from "./components/Profile";
+import StaffRegister from "./pages/StaffRegister";
+import Admin from "./assets/admin/admin";
+import Header from "./assets/admin/Header";
+import Sidebar from "./assets/admin/Sidebar";
+import Adminhome from "./assets/admin/adminhome";
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/userpage" element={<UserPage/>}/>
+        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/adminpanel" element={<Adminpanel/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/admin" element={<Admin/>}/>
+        <Route path="/adminhome" element={<Adminhome/>}/>
+        <Route path="/head" element={<Header/>}/>
+        <Route path="/Sidebar" element={<Sidebar/>}/>
+        <Route path="/staffregister" element={<StaffRegister/>}/>
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
